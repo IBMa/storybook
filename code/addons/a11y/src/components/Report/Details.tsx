ff9eb4c5522ac981dea842080efa4a7c62d65f46
Chance-Gong <chance.gong@ibm.com>
2025-10-10 10:39:27 -0500

    Message before Learn how to...

--- code/addons/a11y/src/components/Report/Details.tsx
+++ code/addons/a11y/src/components/Report/Details.tsx
@@ -142,7 +142,7 @@ export const Details = ({ id, item, type, selection, handleSelectionChange }: De
     <Info>
       <RuleId>{item.id}</RuleId>
       <Description>
-        {getFriendlySummaryForAxeResult(item)}{' '}
+        {item.help}{' '}
         <Link href={item.helpUrl} target="_blank" rel="noopener noreferrer" withArrow>
           Learn how to resolve this violation
         </Link>
